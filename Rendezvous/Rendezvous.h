90add089e485a88a9e06eee59a1685da611a3e1f
fumiko-ishizawa <fumiko-ishizawa@cookpad.com>
2017-09-20 14:47:08 +0900

    Initial Commit

---
+++ Rendezvous/Rendezvous.h
@@ -0,0 +1,19 @@
+//
+//  Rendezvous.h
+//  Rendezvous
+//
+//  Created by fumiko-ishizawa on 2017/09/20.
+//  Copyright © 2017年 fumikoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rendezvous.
+FOUNDATION_EXPORT double RendezvousVersionNumber;
+
+//! Project version string for Rendezvous.
+FOUNDATION_EXPORT const unsigned char RendezvousVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rendezvous/PublicHeader.h>
+
+
